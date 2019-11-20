d6774cf2a7c5d275d07e108ad5e50a4efd45adde
James Fenwick <>
2019-11-20 15:58:12 +0000

    Screenshot Game View

---
+++ Editor/Screenshot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public class Screenshot : EditorWindow
+{
+    [MenuItem("J-Scripts/Screenshot")]
+    public static void Screengrab()
+    {
+        string path = EditorUtility.SaveFilePanelInProject("Screenshot Current Gameview", "Screenshot", "png", "Save?");
+
+        if (path.Length != 0)
+        {
+            ScreenCapture.CaptureScreenshot(path);
+        }
+    }
+}
